f731e2a90461e089479745dcf4e62cb904b6fc00
Herizo Ludovic <herizo.ludovic@gmail.com>
2023-10-30 16:29:16 +0300

    ajax complete

--- assets/javascripts/discourse/initializers/alert.js
+++ assets/javascripts/discourse/initializers/alert.js
@@ -16,6 +16,15 @@ export default {
 
       $('#temporary-respond').text('fooo bar');
     });
+
+    $( document ).on( "ajaxComplete", function() {
+      $( ".log" ).text( "Triggered ajaxComplete handler." );
+      if($('#topic-footer-buttons').find('button').hasClass('create')) {
+        $('.pre-connected').hide();
+      }
+
+      $('#temporary-respond').text('fooo bar');
+    } );
    
   }
 };
