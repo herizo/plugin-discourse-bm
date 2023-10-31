e4a9275ddad72dcc7b02dc3d37a353fe0000864e
Herizo Ludovic <herizo.ludovic@gmail.com>
2023-10-31 09:53:51 +0300

    remove onchange page change etc ..

--- assets/javascripts/discourse/initializers/alert.js
+++ assets/javascripts/discourse/initializers/alert.js
@@ -5,25 +5,6 @@ export default {
   name: 'alert',
   initialize() {
 
-    this.appEvents.on('didTransition', () => {
-      // Insérez votre code ici pour réagir au changement de page
-      console.log('La page a changé. Nouvelle URL : ');
-      if($('#topic-footer-buttons').find('button').hasClass('create')) {
-        $('.pre-connected').hide();
-      }
-
-      $('#reponse-rapide').on('click', function(){
-        $('button.pull-right').trigger('click');
-        $.cookie('temporary-respond',$('#temporary-respond').val() );
-      });
-
-      if(typeof($.cookie('temporary-respond')) !== "undefined"  ) {
-        $('.d-editor-input').val($.cookie('temporary-respond'));
-      }   
-
-    });
-
-
     $(document).ready(function(){
         if($('#topic-footer-buttons').find('button').hasClass('create')) {
             $('.pre-connected').hide();
