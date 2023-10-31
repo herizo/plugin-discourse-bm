89e3f5100c56f64b92d5979e06a53920772087e4
Herizo Ludovic <herizo.ludovic@gmail.com>
2023-10-31 09:21:28 +0300

    event page change

--- assets/javascripts/discourse/initializers/alert.js
+++ assets/javascripts/discourse/initializers/alert.js
@@ -1,6 +1,26 @@
 export default {
   name: 'alert',
   initialize() {
+
+    DiscourseURL.onChanged((url) => {
+      // Insérez votre code ici pour réagir au changement de page
+      console.log('La page a changé. Nouvelle URL : ' + url);
+      if($('#topic-footer-buttons').find('button').hasClass('create')) {
+            $('.pre-connected').hide();
+        }
+
+        $('#reponse-rapide').on('click', function(){
+          $('button.pull-right').trigger('click');
+          $.cookie('temporary-respond',$('#temporary-respond').val() );
+        });
+
+        if(typeof($.cookie('temporary-respond')) !== "undefined"  ) {
+          $('.d-editor-input').val($.cookie('temporary-respond'));
+        }   
+
+    });
+
+    
     $(document).ready(function(){
         if($('#topic-footer-buttons').find('button').hasClass('create')) {
             $('.pre-connected').hide();
