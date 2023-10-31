180a33c61f6a1456e16f534ebb31cfcd36d8cb5f
Herizo Ludovic <herizo.ludovic@gmail.com>
2023-10-31 09:10:51 +0300

    nettoyage de codes

--- assets/javascripts/discourse/initializers/alert.js
+++ assets/javascripts/discourse/initializers/alert.js
@@ -2,28 +2,24 @@ export default {
   name: 'alert',
   initialize() {
     $(document).ready(function(){
-      console.log('ready event triggered');
-    	if($('#topic-footer-buttons').find('button').hasClass('create')) {
-    		$('.pre-connected').hide();
-    	}
+        if($('#topic-footer-buttons').find('button').hasClass('create')) {
+            $('.pre-connected').hide();
+        }
 
-      $('#reponse-rapide').on('click', function(){
-        $('button.pull-right').trigger('click');
-        $.cookie('temporary-respond',$('#temporary-respond').val() );
-      });
+        $('#reponse-rapide').on('click', function(){
+          $('button.pull-right').trigger('click');
+          $.cookie('temporary-respond',$('#temporary-respond').val() );
+        });
 
-      if(typeof($.cookie('temporary-respond')) !== "undefined" && $.cookie('temporary-respond').length > 0  ) {
-       
-        $('.d-editor-input').val($.cookie('temporary-respond'));
-      }
-      
-      
+        if(typeof($.cookie('temporary-respond')) !== "undefined"  ) {
+          $('.d-editor-input').val($.cookie('temporary-respond'));
+        }      
 
     });
 
     $(document).on('click', function(){
 
-      console.log('click event triggered');
+      
       if($('#topic-footer-buttons').find('button').hasClass('create')) {
         $('.pre-connected').hide();
       }
